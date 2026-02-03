2f036feaad5da477d623d7a77c3972fbe7bb186a
ajmal kp <ajmal.kp@marensolutions.com>
2026-02-03 11:57:14 +0530

    Initial directory set-up

---
+++ Movies/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Ajmal KP on 02/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Tab(Constants.homeString, systemImage: Constants.homeIconString){
+                Text(Constants.homeString)
+            }
+            Tab(Constants.upcomingString, systemImage: Constants.upcomingIconString){
+                Text(Constants.upcomingString)
+            }
+            Tab(Constants.searchString, systemImage: Constants.searchIconString){
+                Text(Constants.searchString)
+            }
+            Tab(Constants.downloadString, systemImage: Constants.downloadIconString){
+                Text(Constants.downloadString)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
